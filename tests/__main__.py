d2c4472bfd481aaef4d2b092087f5ca1a3efea83
Bohdon Sayre <bohdon@gmail.com>
2022-11-13 01:01:41 -0500

    apply black formatting and snake case to tests

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,18 +1,16 @@
-
 import sys
 import os
 import unittest
 import maya.standalone
 
 
-def updateSysPaths():
+def update_sys_paths():
     """
-    Update sys.path to contain any missing script paths
-    found in MAYA_SCRIPT_PATH. This must be performed
-    after maya standalone is initialized
+    Update `sys.path` to contain any missing script paths found in MAYA_SCRIPT_PATH.
+    This must be performed after maya standalone is initialized
     """
-    scriptPaths = os.environ['MAYA_SCRIPT_PATH'].split(':')
-    for p in scriptPaths:
+    script_paths = os.environ["MAYA_SCRIPT_PATH"].split(":")
+    for p in script_paths:
         if p not in sys.path:
             sys.path.append(p)
 
@@ -20,6 +18,7 @@ def updateSysPaths():
 def run_tests():
     # lazy loading to wait for maya env to be initialized
     import test_core
+
     suite = unittest.TestSuite()
     suite.addTests(unittest.TestLoader().loadTestsFromModule(test_core))
     unittest.TextTestRunner(verbosity=2).run(suite)
@@ -27,11 +26,11 @@ def run_tests():
 
 def main():
     maya.standalone.initialize()
-    updateSysPaths()
+    update_sys_paths()
     # insert the package to test at beginning of sys path in
     # case its already installed in maya modules path
-    moduleScripts = os.path.join(sys.argv[1], 'scripts')
-    sys.path.insert(0, os.path.abspath(moduleScripts))
+    module_scripts = os.path.join(sys.argv[1], "scripts")
+    sys.path.insert(0, os.path.abspath(module_scripts))
     run_tests()
 
 
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,94 +1,90 @@
-
 import unittest
 import pymel.core as pm
 
 import pymetanode as meta
 
-class TestMetaData(unittest.TestCase):
 
+class TestMetaData(unittest.TestCase):
     def setUp(self):
-        self.node = pm.group(em=True)
+        self.node = pm.group(empty=True)
 
     def tearDown(self):
         pm.delete(self.node)
 
-    def test_setAndGetData(self):
-        setData = ['myData', {'a':1, 'b':2}, ('x', 'y', 'z')]
-        className = 'myMetaClass'
-        meta.setMetaData(self.node, className, setData)
-        self.assertEqual(meta.getMetaData(self.node, className), setData)
+    def test_set_and_get_data(self):
+        set_data = ["myData", {"a": 1, "b": 2}, ("x", "y", "z")]
+        class_name = "myMetaClass"
+        meta.setMetaData(self.node, class_name, set_data)
+        self.assertEqual(meta.getMetaData(self.node, class_name), set_data)
 
-    def test_multiClassData(self):
-        cls1 = 'myMetaClass1'
-        cls2 = 'myMetaClass2'
+    def test_multi_class_data(self):
+        cls1 = "myMetaClass1"
+        cls2 = "myMetaClass2"
         meta.setMetaData(self.node, cls1, None)
         meta.setMetaData(self.node, cls2, None)
         self.assertEqual(meta.getMetaData(self.node), {cls1: None, cls2: None})
 
-    def test_removeData(self):
-        meta.setMetaData(self.node, 'myMetaClass', None)
+    def test_remove_data(self):
+        meta.setMetaData(self.node, "myMetaClass", None)
         self.assertTrue(meta.isMetaNode(self.node))
         result = meta.removeMetaData(self.node)
         self.assertTrue(result)
         self.assertFalse(meta.isMetaNode(self.node))
 
-    def test_removeLockedData(self):
-        meta.setMetaData(self.node, 'myMetaClass', 'myTestData')
+    def test_remove_locked_data(self):
+        meta.setMetaData(self.node, "myMetaClass", "myTestData")
         self.node.attr(meta.core.METADATA_ATTR).setLocked(True)
         result = meta.removeMetaData(self.node)
         self.assertFalse(result)
         data = meta.getMetaData(self.node)
-        self.assertEqual(data, {'myMetaClass': 'myTestData'})
-    
-    def test_removeClassData(self):
-        meta.setMetaData(self.node, 'myMetaClass', None)
-        meta.setMetaData(self.node, 'mySecondMetaClass', None)
-        result = meta.removeMetaData(self.node, 'myMetaClass')
+        self.assertEqual(data, {"myMetaClass": "myTestData"})
+
+    def test_remove_class_data(self):
+        meta.setMetaData(self.node, "myMetaClass", None)
+        meta.setMetaData(self.node, "mySecondMetaClass", None)
+        result = meta.removeMetaData(self.node, "myMetaClass")
         self.assertTrue(result)
         self.assertTrue(meta.isMetaNode(self.node))
-        self.assertFalse(meta.hasMetaClass(self.node, 'myMetaClass'))
-        self.assertTrue(meta.hasMetaClass(self.node, 'mySecondMetaClass'))
+        self.assertFalse(meta.hasMetaClass(self.node, "myMetaClass"))
+        self.assertTrue(meta.hasMetaClass(self.node, "mySecondMetaClass"))
 
-    def test_removeLockedClass(self):
-        meta.setMetaData(self.node, 'myMetaClass', 'myTestData')
-        self.node.attr(meta.core.METACLASS_ATTR_PREFIX + 'myMetaClass').setLocked(True)
+    def test_remove_locked_class(self):
+        meta.setMetaData(self.node, "myMetaClass", "myTestData")
+        self.node.attr(meta.core.METACLASS_ATTR_PREFIX + "myMetaClass").setLocked(True)
         result = meta.removeMetaData(self.node)
         self.assertFalse(result)
         data = meta.getMetaData(self.node)
-        self.assertEqual(data, {'myMetaClass': 'myTestData'})
+        self.assertEqual(data, {"myMetaClass": "myTestData"})
 
 
 class TestNodeFinding(unittest.TestCase):
-
     def setUp(self):
-        self.nodeA = pm.group(em=True)
-        meta.setMetaData(self.nodeA, 'ClassA', 'A')
-        self.nodeB = pm.group(em=True)
-        meta.setMetaData(self.nodeB, 'ClassB', 'B')
-        meta.setMetaData(self.nodeB, 'ClassD', 'D')
-        self.nodeC = pm.group(em=True)
-        meta.setMetaData(self.nodeC, 'ClassC', 'C')
-        meta.setMetaData(self.nodeC, 'ClassD', 'D')
+        self.nodeA = pm.group(empty=True)
+        meta.setMetaData(self.nodeA, "ClassA", "A")
+        self.nodeB = pm.group(empty=True)
+        meta.setMetaData(self.nodeB, "ClassB", "B")
+        meta.setMetaData(self.nodeB, "ClassD", "D")
+        self.nodeC = pm.group(empty=True)
+        meta.setMetaData(self.nodeC, "ClassC", "C")
+        meta.setMetaData(self.nodeC, "ClassD", "D")
 
     def tearDown(self):
         pm.delete([self.nodeA, self.nodeB, self.nodeC])
 
-    def test_findAll(self):
+    def test_find_all(self):
         nodes = meta.findMetaNodes()
         self.assertTrue(self.nodeA in nodes)
         self.assertTrue(self.nodeB in nodes)
         self.assertTrue(self.nodeC in nodes)
 
-    def test_findClassA(self):
-        nodes = meta.findMetaNodes('ClassA')
+    def test_find_class_a(self):
+        nodes = meta.findMetaNodes("ClassA")
         self.assertTrue(self.nodeA in nodes)
         self.assertTrue(self.nodeB not in nodes)
         self.assertTrue(self.nodeC not in nodes)
 
-    def test_findClassD(self):
-        nodes = meta.findMetaNodes('ClassD')
+    def test_find_class_d(self):
+        nodes = meta.findMetaNodes("ClassD")
         self.assertTrue(self.nodeA not in nodes)
         self.assertTrue(self.nodeB in nodes)
         self.assertTrue(self.nodeC in nodes)
-
-
